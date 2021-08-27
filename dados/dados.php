da1d9bd5e692d2f98cfd39f6809268eac410929b
Miguelmichls <schwirkoski@gmail.com>
2021-08-27 04:02:18 -0300

    criação do arquivo dados.php

---
+++ dados/dados.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Los Dulces</title>
+    </head>
+    <body>
+     
+    </body>
+</html>
+
+<?php
+$produtos = [
+    "manga" =>[ 
+      "descricao"=>" Um chef teve a feliz ideia de viajar pelo centro do México e foi inspirado a criar uma receita tropical que leva chardonnay e manga. Certas ideias merecem mesmo ser celebradas, não é verdade?
+
+      ",
+      'preco'=>16.99,
+      
+    ],
+    'bolinho'=>[
+      'descricao'=>"O cheesecake de dulce de leche é um tipo de bolo coberto com nozes picadas e recheado com creme de queijo. Apesar de levar poucos ingredientes e exigir algumas horas para esfriar, a sobremesa tem sabor surpreendente.",
+      'preco'=> 14.99,
+      
+    ], 
+    'gourmet'=>[
+      'descricao'=>"Os maias deixaram herança histórica inquestionável, também, na culinária. Uma delas, acredita-se, é a arte de moer os grãos de cacau para, então, criar o chocolate. Então, nada mais justo do que criar uma torta de chocolate maia, que leva ainda pimenta e canela",
+      'preco'=> 59.99,
+      
+    ],
+  
+    'pimenta'=>[
+      'descricao'=>"O tipo de doce cuja mistura exige certa experiência para equilibrar os sabores. Acredita-se que o bolo de chocolate com pimenta seja uma herança asteca que, então, podem ter sido os primeiros a misturar os dois ingredientes. Abençoados sejam, pois a sobremesa é maravilhosa! ",
+      'preco'=> 39.99,
+      
+    ],
+    
+    
+  ];
+?>
