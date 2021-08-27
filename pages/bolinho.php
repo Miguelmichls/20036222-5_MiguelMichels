bc75246a13a8a7aa2b13794859f073c68653e0c0
Miguelmichls <schwirkoski@gmail.com>
2021-08-27 04:12:38 -0300

    criação da pagina bolinho.php

---
+++ pages/bolinho.php
@@ -0,0 +1,17 @@
+<h2>Cheesecake de Dulce de Leche</h2>
+
+
+
+<div class="main">
+    <aside class="texto">
+        <?php echo $produtos["bolinho"]["descricao"]?>
+    </aside>
+
+    <div>
+    <img class="img-prod" src="./imagens/cheesecake.jpg"  alt="imagem_de_bolos">
+     <div class="botao-valor">R$<?php echo $produtos["bolinho"]["preco"]?></div>
+     <div class="botao-valor"><button  class="comprar">Comprar</button></div>
+    </div>
+</div>
+</body>
+</html>
