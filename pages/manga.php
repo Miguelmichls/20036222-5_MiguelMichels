3d545aef5ddc88b83e5c875de818896cfb39c99c
Miguelmichls <schwirkoski@gmail.com>
2021-08-27 04:16:23 -0300

    criação da pagina manga.php

---
+++ pages/manga.php
@@ -0,0 +1,17 @@
+<h2>Torta de Manga com Chadornnay</h2>
+
+<div class="main">
+    <aside class="texto">
+        <?php echo $produtos["manga"]["descricao"]?>
+    </aside>
+
+    <div>
+        <img class="img-prod"  src="./imagens/manga.jpg"  alt="imagem_de_manga">
+        <div class="botao-valor">R$<?php echo $produtos["manga"]["preco"]?></div>
+        <div class="botao-valor"><button  class="comprar">Comprar</button></div>
+    </div>
+</div>
+
+
+
+</html>
