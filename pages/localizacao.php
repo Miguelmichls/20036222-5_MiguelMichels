30a0705aa214de36d87cfe56937fef387195d704
Miguelmichls <schwirkoski@gmail.com>
2021-08-27 06:50:39 -0300

    adicionado link incorporado do google maps

--- pages/localizacao.php
+++ pages/localizacao.php
@@ -1,6 +1,6 @@
 <h2>Nossa Localização</h2>
 <div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 500px">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d773.1509451287917!2d-45.198504633471316!3d-22.8118582414399!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ccc45a38350ea3%3A0x7e0a81362424c328!2sBuriti%20Shopping%20Guar%C3%A1!5e0!3m2!1spt-BR!2sbr!4v1629589385184!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d521.9231854539156!2d-49.28917323425701!3d-28.35683343438441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x952109341c3291c9%3A0x2290752c6ecebc19!2sR.%20Jos%C3%A9%20Antunes%20Matos%2C%2051%20-%20Centro%2C%20Orleans%20-%20SC%2C%2088870-000!5e0!3m2!1spt-BR!2sbr!4v1630057525310!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 </div>
 
 
