32f4aefa2fc03cbe0876d6ab8c98e692b1b7a607
Miguelmichls <schwirkoski@gmail.com>
2021-08-27 04:16:49 -0300

    criação da pagina pimenta.php

---
+++ pages/pimenta.php
@@ -0,0 +1,20 @@
+<h2>Chocolate com Pimenta</h2>
+
+
+
+
+<div class="main">
+    <aside class="texto">
+    <?php echo $produtos["pimenta"]["descricao"]?>    
+    </aside>
+
+    <div>
+     <img class="img-prod" src="./imagens/pimenta.jpg"  alt="imagem_de_bolos">
+     <div class="botao-valor">R$<?php echo $produtos["pimenta"]["preco"]?></div>
+     <div class="botao-valor"><button  class="comprar">Comprar</button></div>
+    </div>
+</div>
+
+
+</body>
+</html>
