11bc037f077210b4b114e2d635e2c9478eefa963
Miguelmichls <schwirkoski@gmail.com>
2021-08-27 04:14:21 -0300

    criação da pagina home.php

---
+++ pages/home.php
@@ -0,0 +1,39 @@
+<h2>Nossos Produtos</h2>
+
+<div class="produtos">
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/torta.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Torta Maia</h5>
+    <p class="card-text"> Uma torta com graos de cacau moidos , feito do modo traticional Maia!! </p>
+    <a href="./index.php?p=gourmet" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/manga.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title"> Torta de Manga</h5>
+    <p class="card-text">Uma torta que leva Chardonnay e manga , uma mistura inigualável !!</p>
+    <a href="./index.php?p=manga"  class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/pimenta.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Bolo de Chocolate com pimenta</h5>
+    <p class="card-text">Uma herança Asteca , Uma sobremesa dos Deuses !!</p>
+    <a href="./index.php?p=pimenta" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/cheesecake.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Cheesecake de leche</h5>
+    <p class="card-text">Um bolo coberto de noses picadas recheado de creme de queijo !!</p>
+    <a href="./index.php?p=bolinho" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+</div>
