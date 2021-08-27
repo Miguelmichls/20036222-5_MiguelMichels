e96f677de0ab75e92d17ab4aacc2237d175aed5c
Miguelmichls <schwirkoski@gmail.com>
2021-08-27 04:15:33 -0300

    criação da pagina gourmet.php

---
+++ pages/gourmet.php
@@ -0,0 +1,20 @@
+<h2>Torta de Chocolate Maia</h2>
+
+
+
+
+
+<div class="main">
+ <aside class="texto">
+ <?php echo $produtos["gourmet"]["descricao"]?>    
+ </aside>
+
+ <div>
+   <img class="img-prod" src="./imagens/torta.jpg" alt="imagens_de_bolos">
+   <div class="botao-valor">R$<?php echo $produtos["gourmet"]["preco"]?></div>
+   <div class="botao-valor"><button class="comprar">Comprar</button></div>
+ </div>
+</div>
+
+</body>
+</html>
