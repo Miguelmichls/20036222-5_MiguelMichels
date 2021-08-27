fe8f3f7cb254015a3fb4cb5ce44b02a3770fa6d1
Miguelmichls <schwirkoski@gmail.com>
2021-08-27 04:18:10 -0300

    criação da pagina quemsomos.php

---
+++ pages/quemsomos.php
@@ -0,0 +1,23 @@
+<h2>Quem Somos</h2>
+<div class="main">
+
+
+
+<p> A cozinha Mexicana sempre esteve presente em nossa família. 
+Comer e fazer doces nos remete a momentos de celebração e bons momentos em nossas vidas.
+A mesa e a comida sempre foram pretexto para nos reunirmos, para estarmos juntos!
+E éramos em tantos...primos, tios, avós, vizinhos, os meus, 
+os seus e os nossos. Tudo junto e misturado. Quanta alegria em torno da mesa. Quantas lembranças boas.
+Foi com o objetivo de compartilhar esses momentos de alegria com outras pessoas que tivemos o desejo de criar uma loja de Doces Mexicanos.
+A partir da primeira ideia, o sonho foi tomando forma e a nossa dedicação em fazer tudo 
+dar certo nos guiou durante a construção desse projeto.
+Buscamos as receitas de família e outras tantas que descobrimos ao longo da vida,
+em cursos e livros de confeitaria. Foi um processo de profissionalização,
+transformação, testes e adaptação de receitas e uma constante busca pelos melhores ingredientes,
+até que tudo ficasse com o sabor e a qualidade que desejávamos.
+A Los Dulces nasce do desejo de levar às pessoas o sabor verdadeiro dos Doces Mexicanos,  Arriba Muchacho !!!
+</p>
+<img src="./imagens/logo1.png" alt="logo-doceria" style="width: 340px;">
+
+</div>
+
